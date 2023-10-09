0067c883f1d848a0db443747ac2c4171ccc009bb
Iván <imontero22@jaumebalmes.net>
2023-10-09 17:27:41 +0200

    Subido ejercicios del viernes 6/10/2023 y de hoy

--- js/clase4.js
+++ js/clase4.js
@@ -41,27 +41,50 @@ function gradosCF() {
 }
 // Pedir un numero en base 10 . Usando divisiones , restos y bucles. Pasarlo a hexadecimal , octal y binario.
 // https://lasmatesfaciles.com/2021/04/28/convertir-decimal-a-octal/
-function conversionDecimal() {
-    let num;
 
-    do {
-        num = parseInt(prompt("Por favor introduce un numero entero :"));
-    } while (isNaN(num));
+//Esta función pasa un número en base decimal a base binaria , octal y hexadecimal.
+function basesNumericas() {
 
-  //  let n2 = pasarABinari(num);
-   let n8 = pasarAOctal(num);
-    //let n16 = pasarAHex(num);  
-}
+//He hecho el ejercicio con arrays en lugar de hacer 3 funciones para cada tipo de base.
 
-function pasarAOctal(num){
-    let cociente;
-    let resto;
-    let resultado="";
-   // let dividendo = Math.floor(num/8);
-    while( num > 0){
-        resto = Math.floor(num%8)+"";
-        num = Math.floor(num/8);    
-        resultado =`${resto}${resultado}`;
+    //Declaracion de variables
+    let num = parseInt(prompt("Por favor introduce un numero entero :"));
+    let number = [num, num,num]; //Array que almacena 3 veces el mismo input para reiniciar la condicion del while después de cada iteracion del for.
+    let resto = [];
+    let arrayDivisores = [2, 8, 16]; // Array que almacena los divisores de cada base
+    let arraySoluciones=     // Array que almacenará los resultados de las distintas bases.
+        [
+            `El resultado en binario es ${resultado[0]} `,
+            `El resultado en octal es ${resultado[1]}`,
+            `El resultado en Hexadecimal es ${resultado[2]}`,
+        ];
+    
+   
+    //Logica
+    for (let x = 0; x < number.length; x++) {
+        number[x];
+        let resultado = ["", "", ""]; //Variable que acumular el resultado de cada tipo (binario , octal y hexadecimal)
+        while (number[x] > 0) {
+            resto[x] = Math.floor(number[x] % arrayDivisores[x]) + ""; //Concatenar para convertir el número en cadena 
+            number[x] = Math.floor(number[x] / arrayDivisores[x]);
+
+            //Verificamos si el divisor es  16 , ya que el resultado será diferente si esta en Hex porque hay que asignarselo a letras en un nuevo array.
+            if (arrayDivisores[x] == 16) {
+
+                let arrayHex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+                resultado[x] = `${arrayHex[resto[x]]}${resultado[x]}`;
+
+            } else {
+                resultado[x] = `${resto[x]}${resultado[x]}`;
+            }
+
+        }
+        console.log(arraySoluciones[x]);
     }
-    console.log("El resultado en octal es " + resultado);
 }
+
+
+
+
+
+
---
+++ js/clase5.js
@@ -0,0 +1,34 @@
+//Una compañia telefonica cobra 10 centimos por llamada y 5 centimos por minuto
+//Pide las horas , minutos y segundos y di el coste la llamada
+
+function llamadaTelf()
+{   
+    //Inputs
+    let tiempo = [
+         parseInt(prompt("Introduce las horas : ")), //[0]
+         parseInt(prompt("Introduce las minutos : ")), //[1]
+         parseInt(prompt("Introduce los segundos : ")) //[2]
+    ]
+
+    //Pasar todo a minutos
+    let horasAMinutos = Math.floor(tiempo[0]*60) ;
+    let segundosMinuto = Math.floor(tiempo[2]/60);
+    let minutosTotales = tiempo[1] + horasAMinutos + segundosMinuto ;
+
+    //Calculo del importe total
+    let ImporteTotalCentimos = 10 + (minutosTotales * 5);
+    let ImporteEuros = Math.floor(ImporteTotalCentimos/100);
+    let ImporteCentimos = Math.floor(ImporteTotalCentimos%100);
+
+    //Resultado
+    console.log( "El importe total es de " + ImporteEuros + " euros y " + ImporteCentimos + " centimos");
+}
+
+//Programa que pide 3 numeros al usuario y los devuele en hexadecimal y representando un color RGB.
+// Pedir al usuario ( nº Filas y columnas) crear a la página html una tabla dinámica que muestre los números (fila y columna).
+// tr = document.createElement("tr");
+//tr.appendChild(td);
+
+
+
+
